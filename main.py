c627bbd69128ffdb0fa47938f9cefb74dcc590f4
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-08-20 16:06:06 +0300

    implement symbols

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 read a C file and highlight keywords in the code and generate HTML.
 """
 import re
+
+# --- constants ----------------------------------------------------------------
+symbols = {
+    "int": "type",
+    "float": "type",
+    "double": "type",
+    "char": "type",
+    "void": "type",
+    "long": "type",
+    "short": "type",
+    "signed": "type",
+    "unsigned": "type",
+    "if": "control",
+    "else": "control",
+    "while": "control",
+    "for": "control",
+    "do": "control",
+    "switch": "control"
+}
+
